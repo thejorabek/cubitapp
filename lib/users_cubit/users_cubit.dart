9fac94bcdc4d9b02cbdd6dab99db1cb2128a5f26
“[thejorabek]” <boyxorozovjorabek@gmail.com>
2022-09-27 11:40:42 +0500

    State

--- lib/users_cubit/users_cubit.dart
+++ lib/users_cubit/users_cubit.dart
@@ -2,5 +2,16 @@ import 'package:cubitapp/users_cubit/users_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class UsersCubit extends Cubit<UsersState> {
-  UsersCubit() : super(UsersInitial());
+  UsersCubit() : super(const UsersInitial());
+
+  Future<void> getUsers() async {
+    try {
+      emit(const UsersInitial());
+      await Future.delayed(Duration(seconds: 2));
+      final res = await Future.value(['Bir', 'Ikki', 'Uch', 'Tort']);
+      emit(UsersComplete(res));
+    } catch (e) {
+      emit(UsersError('Malumot kelmay qoldi'));
+    }
+  }
 }

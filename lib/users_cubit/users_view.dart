8fb5e797e5e5ca877ecef71be0bd6aaa7bebc8db
“[thejorabek]” <boyxorozovjorabek@gmail.com>
2022-09-27 12:03:44 +0500

    Deleted other files

--- lib/users_cubit/users_view.dart
+++ lib/users_cubit/users_view.dart
@@ -1,3 +1,4 @@
+import 'package:cubitapp/users_cubit/users_cubit.dart';
 import 'package:cubitapp/users_cubit/users_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -12,17 +13,44 @@ class UsersView extends StatefulWidget {
 class _UsersViewState extends State<UsersView> {
   @override
   Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => UsersCubit(),
+      lazy: false,
+      child: myScaffold(),
+    );
+  }
+
+  Scaffold myScaffold() {
     return Scaffold(
       appBar: AppBar(
-        title: Text('user Page'),
+        title: const Text('User Page'),
         centerTitle: true,
       ),
-      body: BlocConsumer(
-        listener: (context, state) {},
+      body: BlocConsumer<UsersCubit, UsersState>(
+        listener: (context, state) {
+          if (state is UsersError) {
+            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                backgroundColor: Colors.red,
+                content: Text('Error while initilalizing...')));
+          } else if (state is UsersComplete) {
+            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                backgroundColor: Colors.green,
+                content: Text('User successfully comleted...')));
+          }
+        },
         builder: (context, state) {
           if (state is UsersInitial) {
             return Center(
-              child: Text('Initial...'),
+              child: Column(
+                children: [
+                  const Text('Initial...'),
+                  ElevatedButton(
+                      onPressed: () {
+                        context.read<UsersCubit>().getUsers();
+                      },
+                      child: const Icon(Icons.refresh))
+                ],
+              ),
             );
           } else if (state is UsersLoading) {
             return const Center(
